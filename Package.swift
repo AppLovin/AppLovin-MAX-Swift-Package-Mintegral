fbd35d5f44efb125ebeedf72d8b36f7ad5ff1b1e
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2026-06-08 11:08:21 -0700

    8.1.5.0.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git", from: "13.0.0"),
-        .package(url: "https://github.com/Mintegral-official/MintegralAdSDK-Swift-Package.git", exact: "8.1.4")
+        .package(url: "https://github.com/Mintegral-official/MintegralAdSDK-Swift-Package.git", exact: "8.1.5")
     ],
     targets: [
         .target(
@@ -28,8 +28,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "AppLovinMediationMintegralAdapter",
-            url: "https://artifacts.applovin.com/ios/com/applovin/mediation/mintegral-adapter/AppLovinMediationMintegralAdapter-8.1.4.0.0.zip",
-            checksum: "45aa617e07256915721740293ced095ce134a7048b2c063c4836dbd5d30aa489"
+            url: "https://artifacts.applovin.com/ios/com/applovin/mediation/mintegral-adapter/AppLovinMediationMintegralAdapter-8.1.5.0.0.zip",
+            checksum: "4b959deef680d4162ccd0653ed669fa7a6e0c7f5a27e52f6fd57be6bc3c757d7"
         )
     ]
 )
